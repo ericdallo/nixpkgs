88bb5b6cb44b6bc7718932a5022363d4158fff84
Eric Dallo <ericdallo06@hotmail.com>
2020-02-13 21:16:21 -0300

    hover: init 2020-01-03

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20434,6 +20434,8 @@ in
     gtk = gtk3;
   };
 
+  hover = callPackage ../development/tools/hover { };
+
   hovercraft = python3Packages.callPackage ../applications/misc/hovercraft { };
 
   howl = callPackage ../applications/editors/howl { };
