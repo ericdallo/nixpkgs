4bd973924272ca80fd133215a8b7a7d52d2d90c8
Eric Dallo <ericdallo06@hotmail.com>
2024-08-16 11:20:18 -0300

    globalprotect-openconnect: 1.4.9 -> 2.3.4

--- pkgs/tools/networking/globalprotect-openconnect/default.nix
+++ pkgs/tools/networking/globalprotect-openconnect/default.nix
@@ -4,23 +4,23 @@
 
 stdenv.mkDerivation rec {
   pname = "globalprotect-openconnect";
-  version = "1.4.9";
+  version = "2.3.4";
 
   src = fetchurl {
     url = "https://github.com/yuezk/GlobalProtect-openconnect/releases/download/v${version}/globalprotect-openconnect-${version}.tar.gz";
-    hash = "sha256-vhvVKESLbqHx3XumxbIWOXIreDkW3yONDMXMHxhjsvk=";
+    hash = "sha256-UmJahTuWAm1WxvBl1aA6dXVoq6HEHYFEzaMzkZnkmoo=";
   };
 
   nativeBuildInputs = [ cmake wrapQtAppsHook ];
 
   buildInputs = [ openconnect qtwebsockets qtwebengine qtkeychain ];
 
-  patchPhase = ''
-    substituteInPlace GPService/gpservice.h \
-      --replace /usr/local/bin/openconnect ${openconnect}/bin/openconnect;
-    substituteInPlace GPService/CMakeLists.txt \
-      --replace /etc/gpservice $out/etc/gpservice;
-  '';
+  # patchPhase = ''
+  #   substituteInPlace GPService/gpservice.h \
+  #     --replace /usr/local/bin/openconnect ${openconnect}/bin/openconnect;
+  #   substituteInPlace GPService/CMakeLists.txt \
+  #     --replace /etc/gpservice $out/etc/gpservice;
+  # '';
 
   meta = with lib; {
     description = "GlobalProtect VPN client (GUI) for Linux based on OpenConnect that supports SAML auth mode";
